312c35c67fed4f94e92c4f883a72155d8751034c
bourbon-beast <steve.g.waters@gmail.com>
2025-04-17 20:06:49 +1000

    ...adding Firestore

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-    plugins: [
-        require('tailwindcss'),
-    ],
+    plugins: [ ],
 })
